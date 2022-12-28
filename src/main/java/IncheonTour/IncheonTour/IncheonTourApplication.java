67bd593b9eb55ed3596bca246dee380757f834d6
ts3083 <ts04031@gmail.com>
2022-12-28 15:39:58 +0900

    Initial commit

---
+++ src/main/java/IncheonTour/IncheonTour/IncheonTourApplication.java
@@ -0,0 +1,13 @@
+package IncheonTour.IncheonTour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncheonTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncheonTourApplication.class, args);
+	}
+
+}
